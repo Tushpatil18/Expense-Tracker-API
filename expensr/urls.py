7d43572910ed5936166b33722fe83bd458632c1e
Tushpatil18 <69032982+Tushpatil18@users.noreply.github.com>
2025-08-21 14:22:53 +0530

    Update urls.py

--- expensr/urls.py
+++ expensr/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", register, name="register"),
     path("login/", login, name="login"),
-    path("", include(router.urls)),  # includes /expenses/ and /expenses/{id}/
+    path("", include(router.urls)),  
     path("summary/monthly/", MonthlySummaryView.as_view(), name="monthly-summary"),
 ]
